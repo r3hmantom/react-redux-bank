d2348e98e80d7aa81e9ad8a68c3fc9b6e379be7f
r3hmantom <r3hmantom@gmail.com>
2024-01-12 16:55:20 +0500

    main: Old way to fetch data from redux store - connect API

--- src/features/accounts/BalanceDisplay.jsx
+++ src/features/accounts/BalanceDisplay.jsx
@@ -1,4 +1,4 @@
-import { useSelector } from "react-redux";
+import { connect } from "react-redux";
 
 function formatCurrency(value) {
   return new Intl.NumberFormat("en", {
@@ -7,9 +7,17 @@ function formatCurrency(value) {
   }).format(value);
 }
 
-function BalanceDisplay() {
-  const balance = useSelector(store => store.account.balance)
+function BalanceDisplay({ balance }) {
   return <div className='balance'>{formatCurrency(balance)}</div>;
 }
 
-export default BalanceDisplay;
+function mapStateToProps(state) {
+  // receives state from store
+  // return object in which we can define the name of the prop that our component should receive
+
+  return {
+    balance: state.account.balance,
+  };
+}
+
+export default connect(mapStateToProps)(BalanceDisplay);
